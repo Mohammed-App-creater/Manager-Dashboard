470fd93b54265a010f878789eafb8c8c5070e03c
Mohammed Ismail <mahammedismail160@gmail.com>
2024-09-28 15:02:13 +0300

    first commit

---
+++ src/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+      productName: 'java',
+      productNumber: "123456",
+      paymentStatus: "due",
+      status: "Pending" 
+    },
+    {
+      productName: 'fullcouse',
+      productNumber: "5782",
+      paymentStatus: "refund",
+      status: "Declined" 
+    },
+    {
+      productName: 'jflex box',
+      productNumber: "456",
+      paymentStatus: "paid",
+      status: "Active" 
+    }
+];
